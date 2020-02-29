5d3edb395a71ea566955ccc8dafce237449c20b4
abdullah <44318011+fx2y@users.noreply.github.com>
2020-02-29 18:29:21 +0700

    playground
    Signed-off-by: abdullah <44318011+fx2y@users.noreply.github.com>

---
+++ playground/chat/chat_test.go
@@ -0,0 +1,39 @@
+package chat_test
+
+import (
+	"github.com/fx2y/learn-web-service-with-golang/playground/chat"
+	"reflect"
+	"testing"
+)
+
+func TestChatRoomBroadcast(t *testing.T) {
+	pub := new(spyPublisher)
+	room := chat.NewRoom(pub)
+	room.AddUser("bob")
+	room.AddUser("alice")
+	_ = room.Broadcast("hi")
+
+	// Check published entries
+	exp := []entry{
+		{user: "bob", message: "hi"},
+		{user: "alice", message: "hi"},
+	}
+
+	if got := pub.published; !reflect.DeepEqual(got, exp) {
+		t.Fatalf("expected the following messages:\n%#+v\ngot:\n%#+v", exp, got)
+	}
+}
+
+type entry struct {
+	user    string
+	message string
+}
+
+type spyPublisher struct {
+	published []entry
+}
+
+func (p *spyPublisher) Publish(user, message string) error {
+	p.published = append(p.published, entry{user: user, message: message})
+	return nil
+}
